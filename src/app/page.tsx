055f94ae2736d7be8c1ac22db089854542951d25
Yasin Junet <hello@yasinjunet.com>
2024-07-27 23:35:53 +0700

    fix: navbar stacking on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,29 @@
 import LayoutContent from "@/components/LayoutContent";
+import LayoutSubmenu from "@/components/LayoutSubmenu";
+import config from "@/config";
 import { Container, View, Text } from "reshaped";
 
 export default function Home() {
   return (
-    <LayoutContent noPadding>
-      <Container width="760px">
-        <View
-          gap={8}
-          padding={{ s: 0, l: 10 }}
-          paddingBlock={{ s: 20, l: 15 }}
-          as="main"
-        >
-          <View gap={1}>
-            <Text as="h1" variant="featured-1" weight="bold">
-              Hello there!
-            </Text>
+    <div className="stacking">
+      <LayoutSubmenu />
+
+      <LayoutContent noPadding>
+        <Container width="760px">
+          <View
+            gap={8}
+            padding={{ s: 0, l: 10 }}
+            paddingBlock={{ s: 20, l: 15 }}
+            as="main"
+          >
+            <View gap={1}>
+              <Text as="h1" variant="featured-1" weight="bold">
+                Hello there!
+              </Text>
+            </View>
           </View>
-        </View>
-      </Container>
-    </LayoutContent>
+        </Container>
+      </LayoutContent>
+    </div>
   );
 }
--- src/app/stack/page.tsx
+++ src/app/stack/page.tsx
@@ -1,25 +1,31 @@
 import React from "react";
-import LayoutContent from "@/components/LayoutContent";
 import { Container, View, Text } from "reshaped";
+import LayoutContent from "@/components/LayoutContent";
+import LayoutSubmenu from "@/components/LayoutSubmenu";
+import config from "@/config";
 
 const Page = () => {
   return (
-    <LayoutContent noPadding>
-      <Container width="760px">
-        <View
-          gap={8}
-          padding={{ s: 0, l: 10 }}
-          paddingBlock={{ s: 20, l: 15 }}
-          as="main"
-        >
-          <View gap={1}>
-            <Text as="h1" variant="featured-1" weight="bold">
-              Stack
-            </Text>
+    <div className="stacking">
+      <LayoutSubmenu title={config.menu[2].title} />
+
+      <LayoutContent noPadding>
+        <Container width="760px">
+          <View
+            gap={8}
+            padding={{ s: 0, l: 10 }}
+            paddingBlock={{ s: 20, l: 15 }}
+            as="main"
+          >
+            <View gap={1}>
+              <Text as="h1" variant="featured-1" weight="bold">
+                Stack
+              </Text>
+            </View>
           </View>
-        </View>
-      </Container>
-    </LayoutContent>
+        </Container>
+      </LayoutContent>
+    </div>
   );
 };
 
