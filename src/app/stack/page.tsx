2b75a33f5c01ce9a7b8a6564b40493c5a4159ab3
Yasin Junet <hello@yasinjunet.com>
2024-07-30 21:10:10 +0700

    style: centered single page

--- src/app/stack/page.tsx
+++ src/app/stack/page.tsx
@@ -1,15 +1,18 @@
 "use client";
 
 import React from "react";
+import { View } from "reshaped";
 import { MDXProvider } from "@mdx-js/react";
 import { components } from "@/components/MdxContent";
 import StackMDX from "@/posts/stack.mdx";
 
 const Page = () => {
   return (
-    <MDXProvider components={components}>
-      <StackMDX />
-    </MDXProvider>
+    <View direction="column" gap={6}>
+      <MDXProvider components={components}>
+        <StackMDX />
+      </MDXProvider>
+    </View>
   );
 };
 
--- src/components/IndexPageContent/IndexPageContent.tsx
+++ src/components/IndexPageContent/IndexPageContent.tsx
@@ -4,12 +4,15 @@ import React from "react";
 import { MDXProvider } from "@mdx-js/react";
 import { components } from "@/components/MdxContent";
 import IndexMDX from "@/posts/index.mdx";
+import { View } from "reshaped";
 
 const IndexPageContent = () => {
   return (
-    <MDXProvider components={components}>
-      <IndexMDX />
-    </MDXProvider>
+    <View direction="column" gap={6}>
+      <MDXProvider components={components}>
+        <IndexMDX />
+      </MDXProvider>
+    </View>
   );
 };
 
